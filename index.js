770e01beb53f119231fb396dbbf4452a107b7626
Zhannie <zkobzaryova@gmail.com>
2017-06-13 19:41:33 +0300

    init

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp'),
-	deploy = require('gulp-gh-pages');
+	deploy = require('gulp-gh-pages')
+	sass        = require('gulp-sass'),
+	concat      = require('gulp-concat'),
+	uglify      = require('gulp-uglifyjs'),
+	cssnano     = require('gulp-cssnano'),
+	rename      = require('gulp-rename'),
+	del         = require('del'), 
+	imagemin    = require('gulp-imagemin'),
+	pngquant    = require('imagemin-pngquant'),
+	cache       = require('gulp-cache'),
+	autoprefixer= require('gulp-autoprefixer');
 
 	gulp.task('deploy', function() {
 		return gulp.src('.dist/**/*')
