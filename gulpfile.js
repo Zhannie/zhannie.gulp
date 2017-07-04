77d84e536c82e1b5cbca778d855551607808e6ab
Zhanna <zkobzaryova@gmail.com>
2017-07-04 22:13:58 +0300

    last update

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var gulp        = require('gulp'),
 	return gulp.src('app/sass/input.scss')
 	.pipe(sass())
 	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-	.pipe(gulp.dest('app/css'))
+	.pipe(gulp.dest('dist/css'))
 	});
 
 	gulp.task('scripts', function() {
@@ -25,7 +25,7 @@ var gulp        = require('gulp'),
 		])
 	.pipe(concat('libs.min.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('app/js'));
+	.pipe(gulp.dest('dist/js'));
 	});
 
 	gulp.task('clear', function() {
@@ -40,10 +40,16 @@ var gulp        = require('gulp'),
 		svgoPlugins: [{removeViewBox:false}],
 		une: [pngquant()]
 	})))
-	.pipe(gulp.dest('app/mimgs'));
+	.pipe(gulp.dest('dist/imgs'));
 	});
 
 	gulp.task('deploy', ['img', 'sass', 'scripts'], function() {
+		var buildCss = gulp.src(['app/css/*'])
+		.pipe(gulp.dest('dist/css'));
+
+		var buildJs = gulp.src(['app/js/script.js'])
+		.pipe(gulp.dest('dist/js'));
+
 		return gulp.src('**/*')
 		.pipe(deploy({
 			repository: 'https://github.com/Zhannie/zhannie.gulp.git'
